1cd92acf7c25d41f811cbca3d2861008b5e810d1
Vishwanathanf22labs <vishwanathan.k.intern@f22labs.com>
2025-06-29 13:33:50 +0530

    Updated

--- backend/controller/product.controller.js
+++ backend/controller/product.controller.js
@@ -47,8 +47,8 @@ exports.fetchAll = async (req, res) => {
 exports.checkoutProducts = async (req, res) => {
   try {
     const instance = new Razorpay({
-      key_id: process.env.KEY_ID,
-      key_secret: process.env.KEY_SECRET,
+      key_id: process.env.RAZORPAY_KEY_ID,
+      key_secret: process.env.RAZORPAY_KEY_SECRET,
     });
 
     const options = {
@@ -75,7 +75,7 @@ exports.verifyCheckout = async (req, res) => {
     const { razorpay_order_id, razorpay_payment_id, razorpay_signature } = req.body;
     const sign = razorpay_order_id + "|" + razorpay_payment_id;
     const expectedSign = crypto
-      .createHmac("sha256", process.env.KEY_SECRET)
+      .createHmac("sha256", process.env.RAZORPAY_KEY_SECRET)
       .update(sign.toString())
       .digest("hex");
 
